780e793853a85bc059a5bdf70b3533dd9b098543
mdrahamatunnisa <n190488@rguktn.ac.in>
2024-12-10 19:58:50 +0530

    initial dashboard front end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
-import RegisterForm from "./components/registration/RegisterForm";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import EmployeeTable from "./components/EmployeeTable";
+import DashboardInsights from "./components/DashboardInsights";
+import "./styles.css";
 
-
-function App() {
-  return (
-    <div className="App">
-      <RegisterForm />
+const App = () => (
+  <Router>
+    <Header />
+    <div className="container mt-4">
+      <Routes>
+        <Route path="/" element={<h2>Welcome to APCPDCL</h2>} />
+        <Route path="/dashboard" element={<DashboardInsights />} />
+        <Route path="/employees" element={<EmployeeTable />} />
+        <Route path="/reports" element={<h2>Reports Section Coming Soon</h2>} />
+      </Routes>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
