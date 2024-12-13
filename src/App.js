5b5e01fe9ea8be3b23b784fc9f5e97955d315973
6302777382 <yamunajth@gmail.com>
2024-12-13 14:15:30 +0530

    integration of registration and login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import RegisterForm from "./Components/registrationForm/RegisterForm";
 import Forgot from "./Components/Forgot/Forgot";
 import LoginForm from "./Components/LoginForm/LoginForm";
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
+        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path="/register" element={<RegisterForm />} />
         <Route path="/forgot-password" element={<Forgot />} />
         <Route path="/login" element={<LoginForm />} />
       </Routes>
